46757e29181d80434fe090bfcb08761f8c7c0a03
mjnrock <mjnrock@umich.edu>
2018-05-31 22:43:06 -0400

    init

---
+++ API.sql
@@ -0,0 +1,36 @@
+USE Hangtime
+GO
+
+--CREATE SCHEMA API
+--GO
+
+--	==============================================
+--		SMART DROP
+--	==============================================
+DECLARE @Schema VARCHAR(255) = 'API';
+DECLARE @SQL NVARCHAR(MAX) = '';
+
+SELECT
+	@SQL = @SQL + 'ALTER TABLE ['+s.name+'].['+t.name+'] DROP CONSTRAINT ['+o.name+'];'
+FROM
+	sys.foreign_key_columns fkc
+	INNER JOIN sys.objects o
+		ON fkc.constraint_object_id = o.object_id
+	INNER JOIN sys.tables t
+		ON fkc.parent_object_id = t.object_id
+	INNER JOIN sys.schemas s
+		ON t.schema_id = s.schema_id
+WHERE
+	s.name = @Schema
+EXEC(@SQL);
+
+SET @SQL = '';
+SELECT
+	@SQL = @SQL + 'IF OBJECT_ID('''+s.name+'.['+t.name+']'') IS NOT NULL DROP TABLE '+s.name+'.['+t.name+'];'
+FROM
+	sys.tables t
+	INNER JOIN sys.schemas s
+		ON t.schema_id = s.schema_id
+WHERE
+	s.name = @Schema
+EXEC(@SQL);
